0cba23d36676a972cbfec8c67c4fe34efcfff1fd
Beef Boolean <ben@benjaminjones.me>
2014-07-06 01:50:21 -0400

    Added transform tools to build vectors

--- dagger2.py
+++ dagger2.py
@@ -1,16 +1,15 @@
 #!/usr/bin/python
-
+# Dagger 2, DAGs from objdump disassembly files
 import sys
 import re
-if __name__=="__main__":
-	args = sys.argv
-	if len(args) == 2:
-		filename = args[1]
-	else:
-		print "Usage: ./dagger2.py filename"
+
+def getSymbols(filename):
+	try:	
+		fp = open(filename)
+	except IOError:
+		print "Could not open " + filename
 		exit(-1)
 
-	fp = open(filename)
 	p = re.compile('\<[\-\._0-9a-zA-Z@]+\>:')
 	p2 = re.compile('\<[\-\._0-9a-zA-Z@]+\>[^ :]')
 	symbols = {}
@@ -25,8 +24,41 @@ if __name__=="__main__":
 			current_symbol = m2[0].strip().strip("<").strip(">")
 			if current_function != "":
 				symbols[current_function].append(current_symbol)
+	return symbols
+def buildVectors(symbols):
+	matrix = [0]*len(symbols)*len(symbols)
+	functions = []
+	vectors = {}
+	for function in symbols:
+		functions.append( function  )
+	for i in range(len(functions)):
+		for j in range(len(functions)):
+			members = symbols[functions[i]]
+			for member in members:
+				if member == functions[j]:
+					matrix[i*len(symbols)+j] += 1
+	index = 0
+	for function in functions:
+		vector = matrix[index*len(functions):index*len(functions)+len(functions)-1]
+		vectors[function] = vector
+		index += 1
+
+	return vectors
 	
-	for symbol in symbols:
-		print symbol + ":"
-		for child in symbols[symbol]:
-			print "\t" + child
+def main():
+	args = sys.argv
+	if len(args) == 2:
+		filename = args[1]
+	else:
+		print "Usage: ./dagger2.py filename"
+		exit(-1)
+
+	symbols = getSymbols(filename)
+	vectors = buildVectors(symbols)
+	
+		
+if __name__=="__main__":
+	main()	
+
+	
+		
