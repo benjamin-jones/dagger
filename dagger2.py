0822ab741abe6b08e282cd2bc092d5354a99bc65
Beef Boolean <ben@benjaminjones.me>
2014-07-05 21:09:56 -0400

    Added objdump parser

---
+++ dagger2.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python
+
+import sys
+import re
+if __name__=="__main__":
+	args = sys.argv
+	if len(args) == 2:
+		filename = args[1]
+	else:
+		print "Usage: ./dagger2.py filename"
+		exit(-1)
+
+	fp = open(filename)
+	p = re.compile('\<[_0-9a-zA-Z@]+\>:')
+	p2 = re.compile('\<[_0-9a-zA-Z@]+\>[^ :]')
+	symbols = {}
+	for line in fp:
+		m = p.findall(line)
+		if len(m) == 1:
+			current_function = m[0].strip(":").strip("<").strip(">")
+			symbols[current_function] = []
+		m2 = p2.findall(line)
+		if len(m2) == 1:
+			current_symbol = m2[0].strip().strip("<").strip(">")
+			symbols[current_function].append(current_symbol)
+	
+	for symbol in symbols:
+		print symbol + ":"
+		for child in symbols[symbol]:
+			print "\t" + child
